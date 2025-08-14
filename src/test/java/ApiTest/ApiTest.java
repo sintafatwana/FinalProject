a52d683bd7395aaeaefb484a613bb3c078ab8966
achso13 <achmadsolehuddin23@gmail.com>
2025-08-14 16:32:45 +0700

    update

--- src/test/java/ApiTest/ApiTest.java
+++ src/test/java/ApiTest/ApiTest.java
@@ -1,4 +1,4 @@
-package api_test;
+package ApiTest;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -10,7 +10,7 @@ import org.junit.Test;
 import java.io.File;
 import static io.restassured.RestAssured.*;
 
-public class Api_test {
+public class ApiTest {
 
     @Test
     public void testGetListUsersValid() {
